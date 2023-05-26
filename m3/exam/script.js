6190820b1b7466ae0e2abbcc0ebf0651c25b646e
Richard-94 <rich.boat82@gmail.com>
2023-05-26 15:33:57 +0200

    exam

--- m3/exam/script.js
+++ m3/exam/script.js
@@ -1,40 +1,65 @@
-var saveCredit = [];
-var callsNumber = [];
-var callsHistory = [];
+;
 var User = /** @class */ (function () {
-    function User(recharge) {
+    function User(recharge, duration) {
         this.recharge = 0;
         this.numberOfCalls = 0;
         this.minuteCost = 0.20;
+        this.saveCredit = [];
+        this.callsHistory = [];
         this.recharge = recharge;
-        saveCredit.push(this.recharge);
+        this.duration = duration;
         var increasedRecharge = this.increaseRecharge(0);
         console.log("your total recharge is £", increasedRecharge);
-        this.totalCalls(100);
+        this.totalCalls(duration);
+        this.totalCalls(duration);
+        this.infoCredit404();
+        //this.resetCalls()
     }
     User.prototype.increaseRecharge = function (rechargeValue) {
         this.recharge += rechargeValue;
-        saveCredit.push(this.recharge);
         return this.recharge;
     };
-    //  callDuration(): number {
-    //      throw new Error("Method not implemented.");
-    //  }
-    // infoCredit404(): number {
-    //     throw new Error("Method not implemented.");
-    // }
     User.prototype.totalCalls = function (duration) {
-        var _this = this;
-        var currentDuration = duration;
-        setInterval(function () {
-            duration++;
-            var cost = _this.minuteCost * duration;
-            callsHistory.push({ duration: duration, cost: cost });
-            console.log("Total calls: ", callsHistory.length);
-            console.log("Call duration: ", duration);
-            console.log("Call cost: £", cost);
-        }, 60000);
+        var cost;
+        this.duration = duration;
+        var durationInMinutes = duration;
+        if (this.recharge <= 0) {
+            console.log("You cannot make a call. Insufficient credit.");
+            return;
+        }
+        else {
+            var cost_1 = (this.minuteCost / 60) * (durationInMinutes * 60);
+            if (cost_1 > this.recharge) {
+                console.log("You cannot make a call. Insufficient credit.");
+                return;
+            }
+            var entry = {
+                id: this.callsHistory.length + 1,
+                duration: durationInMinutes,
+                cost: cost_1,
+            };
+            this.callsHistory.push(entry);
+            console.log(this.callsHistory);
+            console.log("Total calls: ", this.callsHistory.length);
+            console.log("Call duration: ", durationInMinutes, "minutes");
+            console.log("Call cost: £", cost_1);
+            this.recharge -= cost_1;
+        }
+    };
+    User.prototype.infoCredit404 = function () {
+        var totalCost = this.callsHistory.reduce(function (total, entry) { return total + entry.cost; }, 0);
+        var remainingCredit = this.recharge - totalCost;
+        this.saveCredit.push(remainingCredit);
+        console.log("Remaining credit: £", remainingCredit);
+        return remainingCredit;
+    };
+    User.prototype.resetCalls = function () {
+        var reset = this.callsHistory;
+        reset.splice(0);
+        console.log(reset);
     };
     return User;
 }());
-var user = new User(1000);
+var user1 = new User(300, 10);
+var user2 = new User(100, 50);
+var user3 = new User(1000, 3000);
--- m3/exam/script.ts
+++ m3/exam/script.ts
@@ -1,42 +1,45 @@
-const saveCredit:number[]=[]
-const callsNumber:number[]=[]
-const callsHistory: CallEntry[] = [];
 
-interface CallEntry {
-    duration: number;
-    cost: number;
-  }
-  
+
+ interface CallEntry {
+    duration: number
+    cost: number,
+    id: number
+  };
+
 
 interface ISmartphone{
     recharge:number;
-    
     minuteCost:number
-    
     increaseRecharge(rechargeValue:number):number;
-    // callDuration():number;
-    // infoCredit404():number;
-     totalCalls(duration: number, stopAfter: number): void;
-    // resetCalls():number;
-    // knowSales():number;
+    infoCredit404():number;
+     totalCalls(duration: number): void;
+    resetCalls():void;
+    
 }
 
 class User implements ISmartphone{
     recharge: number=0;
     numberOfCalls: number=0;
     minuteCost:number=0.20
+    duration: number;
+    saveCredit:number[]=[]
+
+    callsHistory: CallEntry[] = [];
    
 
 
-    constructor(recharge:number){
-        this.recharge=recharge;
-        saveCredit.push(this.recharge);
-        
+    constructor(recharge:number,duration:number){
+    this.recharge=recharge;
+    this.duration = duration
+    const increasedRecharge = this.increaseRecharge(0)
+    console.log("your total recharge is £",increasedRecharge);
 
-        const increasedRecharge = this.increaseRecharge(0)
-        console.log("your total recharge is £",increasedRecharge);
 
-    this.totalCalls(100)
+    this.totalCalls(duration)
+    this.totalCalls(duration)
+    
+    this.infoCredit404();
+    //this.resetCalls()
          
     }
 
@@ -44,41 +47,67 @@ class User implements ISmartphone{
     increaseRecharge(rechargeValue:number): number {
         
     this.recharge += rechargeValue;
-    saveCredit.push(this.recharge);
+    
     return this.recharge;
  
     }
 
-    
-    
-    //  callDuration(): number {
-    //      throw new Error("Method not implemented.");
-    //  }
 
+   
+   totalCalls(duration: number):void{
+    let cost:number;
+    this.duration=duration
+    const durationInMinutes = duration; 
+    if(this.recharge<=0){
+        console.log("You cannot make a call. Insufficient credit.");
+      return;
+    }else{
+       
+        const cost = (this.minuteCost/60) * (durationInMinutes*60); 
+        if (cost > this.recharge) {
+            console.log("You cannot make a call. Insufficient credit.");
+            return;
+        }
 
-    // infoCredit404(): number {
-    //     throw new Error("Method not implemented.");
-    // }
-    totalCalls(duration: number):void{
-      const durationInMinutes = duration; // Duration is already in minutes
-  const cost = this.minuteCost * (durationInMinutes / 60); // Calculate cost based on minutes
-  callsHistory.push({ duration: durationInMinutes, cost }); // Store duration in minutes
-  console.log("Total calls: ", callsHistory.length);
-  console.log("Call duration: ", durationInMinutes, "minutes");
-  console.log("Call cost: £", cost);
+        const entry: CallEntry = {
+            id: this.callsHistory.length + 1,
+            duration: durationInMinutes,
+            cost: cost,
+            
+          };
+
+        this.callsHistory.push(entry); 
+        console.log(this.callsHistory);
         
-      }
-    // resetCalls(): number {
-    //     throw new Error("Method not implemented.");
-    // }
-    // knowSales(): number {
-    //     throw new Error("Method not implemented.");
-    // }
+        console.log("Total calls: ", this.callsHistory.length);
+        console.log("Call duration: ", durationInMinutes, "minutes");
+        console.log("Call cost: £", cost);  
+    this.recharge -= cost;
+    }
     
 }
 
+      infoCredit404(): number {
+        const totalCost = this.callsHistory.reduce((total, entry) => total + entry.cost, 0);
+    const remainingCredit = this.recharge - totalCost;
+    this.saveCredit.push(remainingCredit);
+    console.log("Remaining credit: £", remainingCredit);
+    return remainingCredit;
+     }
 
-const user = new User(1000);
+ resetCalls(): void {
+   let reset =  this.callsHistory;
+    reset.splice(0)
+    console.log(reset);
+    
+ }
+    
+}
+
+const user1 = new User(300,10);
+const user2 = new User(100,50);
+const user3 = new User(1000,3000);
+
 
 
 
